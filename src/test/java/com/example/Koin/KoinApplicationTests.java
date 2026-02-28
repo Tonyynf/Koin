8b89ac2b67660332b000dce8de22d1ce362a60ad
Tonyynf <eusoulembrado@gmail.com>
2026-02-28 20:47:45 -0300

    first commit

---
+++ src/test/java/com/example/Koin/KoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Koin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
