2c3246d15353e26ee7de3028efdd017664b312da
Tonyynf <tobyencarnacao@gmail.com>
2026-03-03 17:04:23 -0300

    Adicionando funcionalidade para somar o valor das transações

--- src/main/java/com/project/repositories/TransacaoRepository.java
+++ src/main/java/com/project/repositories/TransacaoRepository.java
@@ -1,9 +1,15 @@
 package com.project.repositories;
 
+import com.project.models.TipoTransacao;
 import com.project.models.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.math.BigDecimal;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long>{
+    @Query("SELECT SUM(t.valor) FROM Transacao t WHERE t.tipo = :tipo AND t.conta.id = :contaId")
+    BigDecimal somarPorTipoEConta(TipoTransacao tipo, Long contaId);
 }
+
