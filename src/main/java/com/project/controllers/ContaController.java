9350417c9868e14fd7447efbc3b5aa336d4f8fef
Tonyynf <tobyencarnacao@gmail.com>
2026-03-19 19:42:54 -0300

    Editando Controllers

---
+++ src/main/java/com/project/controllers/ContaController.java
@@ -0,0 +1,17 @@
+package com.project.controllers;
+
+import com.project.models.Transacao;
+import com.project.services.TransacaoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/contas")
+public class ContaController {
+
+
+
+}
--- src/main/java/com/project/controllers/TransacaoController.java
+++ src/main/java/com/project/controllers/TransacaoController.java
@@ -1,10 +1,13 @@
 package com.project.controllers;
 
+import com.project.dto.TransacaoDTO;
 import com.project.models.Transacao;
 import com.project.services.TransacaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/transacoes")
@@ -17,8 +20,21 @@ public class TransacaoController {
     }
 
     @GetMapping("/saldo/{id}")
-    public BigDecimal getSaldoReal(@PathVariable Long id) {
-        return transacaoService.CalcularValorReal(id);
+    public ResponseEntity<Map<String, Object>> getSaldoReal(@PathVariable Long id) {
+        BigDecimal saldo = transacaoService.CalcularValorReal(id);
+
+        Map<String, Object> resposta = Map.of(
+                "contaId", id,
+                "saldo", saldo,
+                "status","sucesso"
+        );
+
+        return ResponseEntity.ok(resposta);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transacao> criarTransacao(@RequestBody TransacaoDTO dados){
+        return 0;
     }
 
     @GetMapping
