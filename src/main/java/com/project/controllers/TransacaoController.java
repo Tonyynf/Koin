1f12c6f85a1a1f3718a658f581763c33b94b7457
Tonyynf <tobyencarnacao@gmail.com>
2026-05-11 18:03:29 -0300

    feat: create find transacao function

--- src/main/java/com/project/controllers/TransacaoController.java
+++ src/main/java/com/project/controllers/TransacaoController.java
@@ -2,23 +2,11 @@ package com.project.controllers;
 
 import com.project.dto.TransacaoRequestDTO;
 import com.project.dto.TransacaoResponseDTO;
-import com.project.models.Categoria;
-import com.project.models.Conta;
-import com.project.models.Transacao;
-import com.project.repositories.CategoriaRepository;
-import com.project.repositories.ContaRepository;
-import com.project.repositories.TransacaoRepository;
 import com.project.services.TransacaoService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/transacoes")
@@ -30,58 +18,24 @@ public class TransacaoController {
     private final TransacaoRepository transacaoRepo;
 
     @GetMapping
-    public List<Transacao> findAll() {
-        return transacaoService.findAllTransacoes();
+    public ResponseEntity<List<TransacaoResponseDTO>> findAllTransacoes() {
+        return ResponseEntity.ok(transacaoService.findAllTransacoes());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<TransacaoResponseDTO> findTransacao(@PathVariable Long id) {
+        return ResponseEntity.ok(transacaoService.findTransacao(id));
     }
     @PostMapping
     public ResponseEntity<Transacao> createTransacao(@RequestBody TransacaoRequestDTO transacaoRequestDTO){
         return transacaoService.createTransacao(transacaoRequestDTO);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<Transacao> updateTransacao(@PathVariable Long id, @RequestBody TransacaoRequestDTO transacaoRequestDTO) {
-        return transacaoService.updateTransacao(id, transacaoRequestDTO);
+    public ResponseEntity<TransacaoResponseDTO> updateTransacao(@PathVariable Long id, @RequestBody TransacaoRequestDTO transacaoRequestDTO) {
+        return ResponseEntity.ok(transacaoService.updateTransacao(id, transacaoRequestDTO));
     }
     @DeleteMapping("/{id}")
-    public void deleteTransacao(@PathVariable Long id){
+    public ResponseEntity<Void> deleteTransacao(@PathVariable Long id){
         transacaoService.deleteTransacao(id);
+        return ResponseEntity.noContent().build();
     }
-
-
-//    @GetMapping("/saldo/{id}")
-//    public ResponseEntity<Map<String, Object>> ConsultarSaldoReal(@PathVariable Long id) {
-//        BigDecimal saldo = transacaoService.CalcularValorReal(id);
-//
-//        Map<String, Object> resposta = Map.of(
-//                "contaId", id,
-//                "saldo", saldo,
-//                "status","sucesso"
-//        );
-//
-//        return ResponseEntity.ok(resposta);
-//    }
-//    @GetMapping("/categoria/{categoriaId}")
-//    public ResponseEntity<List<Transacao>> buscarPorCategoria(@PathVariable Long categoriaId){
-//        List<Transacao> transacoes = transacaoRepo.findByContaId(categoriaId);
-//        return ResponseEntity.ok(transacoes);
-//    }
-//    @GetMapping("/conta/{contaId}")
-//    public ResponseEntity<List<Transacao>> buscarPorConta(@PathVariable Long contaId){
-//        List<Transacao> transacoes = transacaoRepo.findByContaId(contaId);
-//        return ResponseEntity.ok(transacoes);
-//    }
-//    @GetMapping("/conta/{contaId}")
-//    public ResponseEntity<List<Transacao>> listarTransacoesPorData(@PathVariable Long contaId){
-//        List<Transacao> transacoes = transacaoRepo.findByContaIdOrderByDataDesc(contaId);
-//        return ResponseEntity.ok(transacoes);
-//    }
-//    @GetMapping("/conta/{contaId}")
-//    public ResponseEntity<List<Transacao>> listarTransacoesPorCategoriaEValor(@PathVariable Long contaId, Long categoriaId){
-//        List<Transacao> transacoes = transacaoRepo.findByContaIdAndCategoriaIdOrderByValorDesc(contaId, categoriaId);
-//        return ResponseEntity.ok(transacoes);
-//    }
-//    @GetMapping("/conta/{contaId}")
-//    public ResponseEntity<List<Transacao>> listarTransacoesValorEspecifico(@PathVariable Long contaId, BigDecimal valor){
-//        List<Transacao> transacoes = transacaoRepo.findByContaIdAndValorGreaterThanEqual(contaId, valor);
-//        return ResponseEntity.ok(transacoes);
-//    }
 }
--- src/main/java/com/project/services/TransacaoService.java
+++ src/main/java/com/project/services/TransacaoService.java
@@ -25,7 +25,7 @@ public class TransacaoService {
     private final ContaRepository contaRepository;
     private final TransacaoRepository transacaoRepository;
 
-    public ResponseEntity<Transacao> createTransacao(@RequestBody TransacaoRequestDTO dados){
+    public TransacaoResponseDTO createTransacao(TransacaoRequestDTO dados){
         Conta conta = contaRepository.findById(dados.contaId()).orElseThrow(() -> new RuntimeException("Conta não encontrada!"));
 
         Categoria categoria = categoriaRepository.findById(dados.categoriaId()).orElseThrow(() -> new RuntimeException("Categoria não encontrada!"));
@@ -40,14 +40,24 @@ public class TransacaoService {
                 categoria
         );
 
-        return ResponseEntity.ok(transacaoRepository.save(novaTransacao));
+        return converterParaResponseDto(transacaoRepository.save(novaTransacao));
     }
 
-    public List<Transacao> findAllTransacoes(){
-        return transacaoRepository.findAll();
+    public List<TransacaoResponseDTO> findAllTransacoes(){
+        return transacaoRepository.findAll()
+                .stream()
+                .map(this::converterParaResponseDto)
+                .toList();
     }
 
-    public ResponseEntity<Transacao> updateTransacao(Long id, TransacaoRequestDTO transacaoRequestDTO){
+    public TransacaoResponseDTO findTransacao(Long id){
+        Transacao transacao = transacaoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Transação não encontrada!"));;
+
+        return converterParaResponseDto(transacao);
+    }
+
+    public TransacaoResponseDTO updateTransacao(Long id, TransacaoRequestDTO transacaoRequestDTO){
         Transacao transacaoExistente = transacaoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Transação não encontrada!"));
 
@@ -63,7 +73,7 @@ public class TransacaoService {
         transacaoExistente.setConta(conta);
         transacaoExistente.setCategoria(categoria);
 
-        return ResponseEntity.ok(transacaoRepository.save(transacaoExistente));
+        return converterParaResponseDto(transacaoRepository.save(transacaoExistente));
     }
 
     public void deleteTransacao(Long id){
@@ -74,24 +84,6 @@ public class TransacaoService {
         transacaoRepository.deleteById(id);
     }
 
-
-//    public List<TransacaoResponseDTO> buscarExtrato(Long contaId, BigDecimal min) {
-//        List<Transacao> listaDoBanco;
-//
-//        if (min == null) {
-//            // Busca simples por conta
-//            listaDoBanco = transacaoRepository.findByContaIdOrderByDataDesc(contaId);
-//        } else {
-//            // Busca com filtro de valor mínimo
-//            listaDoBanco = transacaoRepository.findByContaIdAndValorGreaterThanEqual(contaId, min);
-//        }
-//
-//        // A mágica do Stream: transforma cada Transacao em TransacaoDTO
-//        return listaDoBanco.stream()
-//                .map(this::converterParaResponseDto)
-//                .toList();
-//    }
-//
     public BigDecimal CalcularValorReal(Long contaId){
         Conta conta = contaRepository.findById(contaId).orElseThrow();
 
@@ -104,32 +96,13 @@ public class TransacaoService {
 
         return conta.getSaldoInicial().add(totalEntradas).subtract(totalSaidas);
     }
-//
-//    public List<Transacao> buscaPorConta(Long contaId) {
-//        return transacaoRepository.findByContaId(contaId);
-//    }
-//    public List<Transacao> buscaPorCategoria(Long categoriaId){
-//        return transacaoRepository.findByCategoriaId(categoriaId);
-//    }
-//
-//    public List<Transacao> listarTransacoesPorData(Long contaId){
-//        return transacaoRepository.findByContaIdOrderByDataDesc(contaId);
-//    }
-//    public List<Transacao> listarTransacoesPorCategoriaEValor(Long contaId, Long categoriaId) {
-//        return transacaoRepository.findByContaIdAndCategoriaIdOrderByValorDesc(contaId, categoriaId);
-//    }
-//    public List<Transacao> listarTransacoesValorEspecifico(Long contaId, BigDecimal valor){
-//        return transacaoRepository.findByContaIdAndValorGreaterThanEqual(contaId, valor);
-//    }
-//
-//    private TransacaoRequestDTO converterParaRequestDto(Transacao t) {
-//        return new TransacaoRequestDTO(
-//                t.getDescricao(),
-//                t.getValor(),
-//                t.getTipo(),
-//                t.getConta().getId(),
-//                t.getCategoria().getId()
-//        );
-//    }
-//
+
+    //converter requisição para DTO
+    private TransacaoResponseDTO converterParaResponseDto(Transacao t) {
+        return new TransacaoResponseDTO(
+                t.getId(), t.getDescricao(), t.getValor(),
+                t.getTipo(), t.getData(),
+                t.getCategoria().getNome(), t.getConta().getNome()
+        );
+    }
 }
