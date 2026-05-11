5e81f33654c1b991633d170ea3409f698ada9a63
Tonyynf <tobyencarnacao@gmail.com>
2026-05-11 18:06:09 -0300

    refactor: replace @NotBlank with @NotNull on request DTO

--- src/main/java/com/project/dto/TransacaoRequestDTO.java
+++ src/main/java/com/project/dto/TransacaoRequestDTO.java
@@ -9,18 +9,18 @@ public record TransacaoRequestDTO(
         @NotBlank(message = "A transação precisa de uma descrição")
         String descricao,
 
-        @NotBlank(message = "O valor não pode ser nulo")
+        @NotNull(message = "O valor não pode ser nulo")
         @Positive(message = "O valor deve ser acima de 0")
         BigDecimal valor,
 
-        @NotBlank(message = "A transação precisa de um tipo")
+        @NotNull(message = "A transação precisa de um tipo")
         TipoTransacao tipo,
 
-        @NotBlank(message = "O ID não pode ser nulo")
+        @NotNull(message = "O ID não pode ser nulo")
         @Positive(message = "O ID deve ser acima de 0")
         Long contaId,
 
-        @NotBlank(message = "O ID não pode ser nulo")
+        @NotNull(message = "O ID não pode ser nulo")
         @Positive(message = "O ID deve ser acima de 0")
         Long categoriaId
 ) {}
